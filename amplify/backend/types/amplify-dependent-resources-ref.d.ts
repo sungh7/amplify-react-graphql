2e67dc2f84034124a24c7a14b312df97dfc9c0f0
sungh7 <50727298+sungh7@users.noreply.github.com>
2024-01-15 14:40:47 +0900

    Added graphql and storage

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,26 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amplifyreactgraphql": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "amplifyreactgraphqle98ca909": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "s3d9bd4fd5": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
